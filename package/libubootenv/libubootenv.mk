50f8b39a77b7768958291ee219b6442da56016a6
Pierre-Jean Texier <pjtexier@koncepto.io>
2019-08-01 20:01:49 +0200

    package/libubootenv: bump to version d63e76fdf3fcab4f260c8cfe9eac745807976d0a
    Also
       - add hash for license file
       - remove patches (already in version)
       - backport patch
    
    Signed-off-by: Pierre-Jean Texier <pjtexier@koncepto.io>

--- package/libubootenv/libubootenv.mk
+++ package/libubootenv/libubootenv.mk
@@ -4,9 +4,10 @@
 #
 ################################################################################
 
-LIBUBOOTENV_VERSION = 8a7d4030bcb106de11632e85b6a0e7b7d4cb47af
+LIBUBOOTENV_VERSION = d63e76fdf3fcab4f260c8cfe9eac745807976d0a
 LIBUBOOTENV_SITE = $(call github,sbabic,libubootenv,$(LIBUBOOTENV_VERSION))
 LIBUBOOTENV_LICENSE = LGPL-2.1
+LIBUBOOTENV_LICENSE_FILES = Licenses/lgpl-2.1.txt
 LIBUBOOTENV_INSTALL_STAGING = YES
 LIBUBOOTENV_DEPENDENCIES = zlib
 
